8fcb8489c839d23f98bb8708ae2ffc192bf4e8de
regitabish <shiba.taiga.lab@gmail.com>
2024-07-26 17:34:05 +0900

    sss

---
+++ AC/ABC/360/c.cpp
@@ -0,0 +1,15 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >>n;
+
+    vector<int> a(n),w(n);
+    for(int i=0;i<n;i++)cin >> a.at(i);
+    for(int i=0;i<n;i++)cin >> w.at(i);
+
+    
+}
