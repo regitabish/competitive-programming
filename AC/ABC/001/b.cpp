fb832330044b75efb4d459e24db27be9669681b4
regitabish <shiba.taiga.lab@gmail.com>
2024-08-29 21:23:55 +0900

    startABC001

---
+++ AC/ABC/001/b.cpp
@@ -0,0 +1,16 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int deg,dis;
+    cin >> deg >> dis;
+
+    if(dis == 0){
+        cout << "C" << " " << dis << endl;
+        return 0;
+    }
+
+    
+}
