25a5e0fe237bc39edbb0c827dea99fa00a814823
regitabish <shiba.taiga.lab@gmail.com>
2024-09-10 07:11:59 +0900

    drill and abc364-d

--- AC/ABC/364/d.cpp
+++ AC/ABC/364/d.cpp
@@ -18,10 +18,39 @@ int main() {
 
     sort(a.begin(),a.end());
 
+    vector<int> ans(q,-1);
+
     for(int i=0;i<q;i++){
         int b;  cin >> b;
         int k;  cin >> k;
-        
+
+        auto isTherePointsThanKInD = [&](int d)->bool{
+            //ある距離dを与えて区間[b-d,b-d]にaの点がK個以上あるかを判定するlambda式
+            
+            auto r=upper_bound(a.begin(),a.end(),b+d);
+            auto l=lower_bound(a.begin(),a.end(),b-d);
+            return r-l>=k;
+        };
+
+        //ここ以下で二分探索で距離dを求める
+        long long ok,ng;
+        ok=0;
+        ng=10e8;
+        while(abs(ok-ng)>1){
+            int dist;
+            dist=(ok+ng)/2;
+            if(isTherePointsThanKInD(dist)){
+                //pointがK個以上あるので距離dは大きい。
+                ng=dist;
+            }else{
+                ok=dist;
+            }
+        }
+
+        ans.at(i)=ok;
     }
+
+    for(int i=0;i<q;i++)cout << ans.at(i) << endl;
+    return 0;
 
 }
