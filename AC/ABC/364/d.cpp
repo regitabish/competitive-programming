34629c770aa1c3fd7726fe1994d00674839722ef
regitabish <shiba.taiga.lab@gmail.com>
2024-09-09 21:47:37 +0900

    20-23

--- AC/ABC/364/d.cpp
+++ AC/ABC/364/d.cpp
@@ -21,29 +21,7 @@ int main() {
     for(int i=0;i<q;i++){
         int b;  cin >> b;
         int k;  cin >> k;
-        long long dl,dr,dkey;
-        dl = 0;
-        dr = 10e9;
-        dkey = (dl+dr)/2;
-        long long l,r;//左右それぞれの添え字
-
-        while(1){
-            l = upper_bound(a.begin(),a.end(),b-dkey) - a.begin();
-            r = lower_bound(a.begin(),a.end(),b-dkey) - a.begin();
-            //cout << l << " " << r << endl;
-            if(r==l)break;
-            if(r-l<k){
-                //dが大きすぎる
-                dkey=dr;
-            }else{
-                //dが小さい
-                dkey=dl;
-            }
-        }
-
-        cout << max(abs(a.at(r)-b),abs(b-a.at(l))) <<endl;
         
     }
-
 
 }
