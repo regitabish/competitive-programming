8b01b96ab2c7a4d5e631afe02470d0d593eae5c1
regitabish <shiba.taiga.lab@gmail.com>
2024-09-26 23:44:20 +0900

    t

---
+++ AC/ABC/372/a.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+    for(auto c:s){
+        if(c!='.')cout << c;
+    }
+    cout << endl;
+}
---
+++ AC/drill/65.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,k;    cin >> n >> k;
+    vector<vector<int>> to(n);
+    for(int i=0;i<n-1;i++){
+        int a,b;
+        cin >> a >> b;
+        a--;
+        b--;
+        to[a].push_back(b);
+        to[b].push_back(a);
+    }
+
+    vector<int> v;
+    for(int i=0;i<k;i++){
+        int tmp;    cin >>tmp;
+        tmp--;
+        v.push_back(tmp);
+    }
+
+    vector<bool> selected(n,false);
+
+    for(int i=0;i<k;++i)selected[v[i]] = true;
+    
+    vector<long long> score(n,0);
+
+    auto dfs = [&](auto dfs,int now,int prev=-1)->void{
+        if(selected[now]==true)score[now]++;
+        
+        for(auto next:to[now]){
+            if(next == prev)continue;
+            dfs(dfs,next,now);
+            score[now]+=score[next];
+        }
+    };
+
+    dfs(dfs,v[0]);
+
+    long long ans = 0;
+    for(auto a:score){
+        if(a>0)ans++;
+    }
+    cout << ans << endl;
+}
