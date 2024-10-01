ad84f982591ad685a66705166c8ff9a11dc3ba71
regitabish <shiba.taiga.lab@gmail.com>
2024-10-02 08:01:08 +0900

    71

---
+++ AC/drill/71.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;  cin >> n;
+    long long t=0;
+    for(int i=0;i<n;++i){
+        int h;  cin >> h;
+        t+=(h/5)*3;
+        h=h%5;
+        while(h>0){
+            t++;
+            if(t%3==0){
+                h-=3;
+            }
+            else{
+                h-=1;
+            }
+        }
+    }
+    cout << t << endl;
+}
