5f2156d02f990fae143bb7ab03d96dc70ec207b6
regitabish <shiba.taiga.lab@gmail.com>
2024-10-02 07:48:23 +0900

    10/02 morningi

--- AC/drill/70.cpp
+++ AC/drill/70.cpp
@@ -1,8 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
-vector<int> divs(int n){
-    vector<int> ans;
-    for(int i=2;i*i<=n){
+vector<long long> divs(long long n){
+    vector<long long> ans;
+    for(int i=1;i*i<=n;++i){
         if(n%i == 0){
             ans.push_back(i);
             if(n/i != i){
@@ -13,11 +13,25 @@ vector<int> divs(int n){
     return ans;
 }
 int main(){
-    int n,q;    cin >> n >> q;
-    vector<int> a(n);
+    int n;    cin >> n ;
+    vector<long long> a(n);
+    map<int,int>mp;
     for(int i=0;i<n;++i){
         cin >> a[i];
-
+        mp[a[i]]++;
     }
-    
+    long long ans = 0;
+    //cout << 9999999999 <<endl;
+    for(int i=0;i<n;++i){
+        long long ai = a.at(i);
+        vector<long long> v = divs(ai);
+        //mp[a[i]]--;
+        for(auto aj:v){
+            ans+=1LL*mp[aj]*mp[ai/aj];
+        }
+        //mp[a[i]]++;
+    }
+
+    cout << ans << endl;
+
 }
