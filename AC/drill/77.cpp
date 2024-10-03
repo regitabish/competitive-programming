30921fabfdf819f6b99a524ef3a3390d64202dd4
regitabish <shiba.taiga.lab@gmail.com>
2024-10-04 07:38:19 +0900

    10/4 morning

--- AC/drill/77.cpp
+++ AC/drill/77.cpp
@@ -3,26 +3,19 @@ using namespace std;
 
 int main(){
     int n,q;    cin >> n >> q;
+    vector<int> a(n);
     map<int, vector<int>>mp;
-
     for(int i=0;i<n;++i){
-        int a;  cin >> a;
-        mp[a].push_back(i+1);
+        cin >> a[i];
+        mp[a[i]].push_back(i+1);
     }
     vector<int> ans;
     
-    for(int i=0;i<q;++i){
+    for(int i=0;i<q;i++){
         int x,k;    cin >> x >> k;
-        //cout << mp[x].size() << endl;
-        if(mp[x].size()<k){
-            ans.push_back(-1);
-        }
-        else{
-            ans.push_back(mp[x].at(k-1));
-        }
+        if(mp[x].size()<k)ans.push_back(-1);
+        else ans.push_back(mp[x][k-1]);
     }
-    
     for(auto a:ans)cout << a << "\n";
     return 0;
-
 }
