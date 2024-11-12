81f151efbeb16df09ca5a42c24fdaa2daab73b24
regitabish <shiba.taiga.lab@gmail.com>
2024-11-12 22:16:42 +0900

    2024/11/12

---
+++ leetCode/arai60/31.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* dummy = new ListNode(0, head);
+        ListNode* ptr = dummy;
+        while (ptr->next != nullptr && ptr->next->next != nullptr) {
+            if (ptr->next->val == ptr->next->next->val) {
+                ListNode* copy = ptr->next;
+                while (copy->next != nullptr && copy->val == copy->next->val) {
+                    copy = copy->next;
+                }
+                ptr->next = copy->next;
+            } else {
+                ptr = ptr->next;
+            }
+        }
+        return dummy->next;
+    }
+};
