e93dfd03afacbe922f5b439d99674bb67faf65eb
regitabish <shiba.taiga.lab@gmail.com>
2025-04-01 07:47:41 +0900

    2025/04/01

---
+++ leetCode/arai60/107.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        if(!root)return {};
+        queue<TreeNode*> q;
+        q.push(root);
+        vector<vector<int>> ans;
+        while(!q.empty()){
+            vector<int> level;
+            int size = q.size();
+            while(size--){
+                level.push_back(q.front()->val);
+                if(q.front()->left)q.push(q.front()->left);
+                if(q.front()->right)q.push(q.front()->right);
+                q.pop();
+
+            }
+            ans.push_back(level);
+        }
+        return ans;
+    }
+};
